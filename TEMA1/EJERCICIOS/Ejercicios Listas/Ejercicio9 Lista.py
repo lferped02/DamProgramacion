913613fbb72d3a75085c46210312cf16f19ea134
lferped02 <lidialilifernandezpedrosa@gmail.com>
2025-10-30 08:41:32 +0100

    Add files via upload

--- TEMA1/EJERCICIOS/Ejercicios Listas/Ejercicio9 Lista.py
+++ TEMA1/EJERCICIOS/Ejercicios Listas/Ejercicio9 Lista.py
@@ -3,11 +3,20 @@ print("Introduce 15 números:")
 for i in range(15):
     numero = int(input(f"Número {i + 1}: "))
     numeros.append(numero)
-ultimo = numeros[-1]
 
-for i in range(14, 0, -1):
-    numeros[i] = numeros[i - 1]
-numeros[0] = ultimo
+cantidad = int(input("Dime el número de veces a rotar: "))
+
+# Rotar manualmente hacia la derecha 'cantidad' veces
+for _ in range(cantidad):
+    ultimo = numeros[-1]
+    for i in range(14, 0, -1):
+        numeros[i] = numeros[i - 1]
+    numeros[0] = ultimo
 
 print("Lista rotada:")
 print(numeros)
+
+# Mostrar los primeros 'cantidad' números que fueron movidos al frente
+print(f"Últimos {cantidad} números que rotaron al frente:")
+for i in range(cantidad):
+    print(numeros[i])
