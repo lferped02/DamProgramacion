79f66386765d7f4e2d92c04f2d1e86fcf779b0f1
lferped02 <lidialilifernandezpedrosa@gmail.com>
2025-10-30 09:55:16 +0100

    Delete TEMA1/Ejercicio5 Cadenas.py

--- TEMA1/Ejercicio5 Cadenas.py
+++
@@ -1,8 +0,0 @@
-numero = "1234567"
-numeroEntero = "5"
-posicion = numero.find(numeroEntero)
-
-if posicion != -1:
-    print("El número se encuentra en la posición:", posicion)
-else:
-    print("El número no está en la cadena.")
