bb8d3e3bc03403b49bd4c3887ff54c8b2ef1ab4c
lferped02 <lidialilifernandezpedrosa@gmail.com>
2025-10-30 09:47:48 +0100

    Delete TEMA1/EJERCICIOS/Ejercicios Cadenas/Ejercicio1 Lista.py

--- TEMA1/EJERCICIOS/Ejercicios Cadenas/Ejercicio1 Lista.py
+++
@@ -1,7 +0,0 @@
-animales = ["Elefante", "Perro", "Gato"]
-animal = input("Introduce el nombre de un animal: ").lower()
-if animal in animales:
-    print ("El animal está en la lista")
-    print (animales.index(animal))
-else:
-    print ("El animal no está en la lista")
