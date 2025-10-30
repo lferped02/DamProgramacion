667afad8c9f5c3e6505810fbe853ae05329955a2
lferped02 <lidialilifernandezpedrosa@gmail.com>
2025-10-30 09:53:02 +0100

    Ejercicios Cadenas

--- TEMA1/EJERCICIOS/Ejercicios Cadenas/Ejercicio4 Cadenas.py
+++ TEMA1/EJERCICIOS/Ejercicios Cadenas/Ejercicio4 Cadenas.py
@@ -1,16 +1,11 @@
 numero = 123456789
-numeros = 4 
+posicion = 4  # Contando desde la derecha, empezando en 0
 
-temperatura = numero
-contador = 0
-while temperatura > 0:
-    temperatura //= 10
-    contador += 1
-veces = contador - 1 - numeros
-
-for i in range(veces):
+# Eliminamos los últimos 'posicion' dígitos
+for _ in range(posicion):
     numero //= 10
 
-digito = numero // 10 % 10 if veces == 0 else numero % 10
+# El dígito deseado es el último ahora
+digito = numero % 10
 
-print("El dígito en la posición", numeros, "es:", digito)
+print("El dígito en la posición", posicion, "es:", digito)
