d85d375cb0f01b5f0e5797429fb9c27817493126
lferped02 <lidialilifernandezpedrosa@gmail.com>
2025-10-30 09:38:23 +0100

    Ejercicios Cadenas

---
+++ TEMA1/EJERCICIOS/Ejercicios Cadenas/Ejercicio6 Lista.py
@@ -0,0 +1,25 @@
+numero = []
+
+# Pedir 10 números por teclado
+for i in range(10):
+    numeros = int(input("Introduce el número: "))
+    numero.append(numeros)
+
+# Encontrar el maximo y el minimo
+maximo = numero [0]
+minimo = numero [0]
+
+for numeros in numero:
+    if numeros>maximo:
+        maximo = numeros
+    elif numeros<minimo:
+        minimo = numeros
+print ("Numeros introducidos: ")
+
+for numeros in numero:
+    if numeros == maximo:
+        print (numeros, "→ máximo")
+    elif numeros == minimo:
+        print(numeros, "→ mínimo")
+    else:
+        print(numeros)
