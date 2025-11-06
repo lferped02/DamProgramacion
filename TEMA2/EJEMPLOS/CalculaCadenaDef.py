51ea914adb0566457fff4674044817b0f93fe9f2
lferped02 <lidialilifernandezpedrosa@gmail.com>
2025-11-06 08:50:51 +0100

    Funciones Ejemplos

--- TEMA2/EJEMPLOS/CalculaCadenaDef.py
+++ TEMA2/EJEMPLOS/CalculaCadenaDef.py
@@ -1,7 +1,4 @@
-def calculaCadena(posicion1, posicion2, numero):
-    salida1 = numero[posicion1]+numero[posicion2]
-    print(salida)
-    return salida1, "Hola"
+import Funciones
 
 #Lee y validar datos
 cadena = input ("Introduce una cadena que debe tener mas de 4 caracteres:")
@@ -14,14 +11,16 @@ while len(cadena)<=4:
 #Comprueba si es par
 if numero%2==0:
     #salida = cadena[2]+cadena[4]
-    calculaCadena(2,4,cadena)
-    output = calculaCadena(2,4,cadena)
+    #calculaCadena(2,4,cadena)
+    output = Funciones.calculaCadena(2,4,cadena)
     print(output)
 #Comprueba si es multiplo de 3
 elif numero %3==0:
     #salida = cadena[1]+cadena[2]
-    calculaCadena(1,2,cadena)
+    #calculaCadena(1,2,cadena)
+    output = Funciones.calculaCadena(1,2,cadena)
 #Comprueba si es multiplo de 7
 elif numero%7==0:
     #salida = cadena[0]+cadena[3]
-    calculaCadena(0,3,cadena)
+    #calculaCadena(0,3,cadena)
+    output = Funciones.calculaCadena(0,3,cadena)
---
+++ TEMA2/EJEMPLOS/Funciones.py
@@ -0,0 +1,4 @@
+def calculaCadena(posicion1, posicion2, numero):
+    salida1 = numero[posicion1]+numero[posicion2]
+    print(salida)
+    return salida1, "Hola"
