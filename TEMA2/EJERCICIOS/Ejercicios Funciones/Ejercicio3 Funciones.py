7a193a2e206d480a05cd806013cac24ca1115f85
lferped02 <lidialilifernandezpedrosa@gmail.com>
2025-11-06 09:51:15 +0100

    Delete TEMA2/EJERCICIOS/Ejercicios Funciones/Ejercicio3 Funciones.py

--- TEMA2/EJERCICIOS/Ejercicios Funciones/Ejercicio3 Funciones.py
+++
@@ -1,8 +0,0 @@
-def convertir_espaciado(cadena):
-    cadenaResultado = ""
-    for c in cadena:
-        if c != '':
-            cadenaResultado += c + " "
-    return cadenaResultado.strip()  # Elimina el espacio final
-
-print(convertir_espaciado("Hola, tú"))
