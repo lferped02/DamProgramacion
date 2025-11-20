1677634ed4f1071551a6deed33c078c78aca9647
lferped02 <lidialilifernandezpedrosa@gmail.com>
2025-11-20 08:33:09 +0100

    Ejercicios Funciones (parte2)

--- TEMA2/EJERCICIOS/Ejercicios Funciones (parte2)/Ejercicio5 Funciones.py
+++ TEMA2/EJERCICIOS/Ejercicios Funciones (parte2)/Ejercicio5 Funciones.py
@@ -1,18 +1,24 @@
 #Esta ordenada 
 def estaOrdenada(lista, ascendente=True):
     i = 0
-    while i < len(lista) - 1:
+    ordenada=True
+    while i < len(lista) - 1 and ordenada:
         if ascendente:  # comprobar ascendente
             if lista[i] > lista[i + 1]:
-                return False
+                ordenada=False
         else:  # comprobar descendente
             if lista[i] < lista[i + 1]:
-                return False
+                ordenada=False
         i += 1
-    return True
+    return ordenada
 
 #Esta ordenado
-print(estaOrdenada([1, 2, 3, 4, 5], True))
+print(estaOrdenada([1, 2, 3, 4, 5], False))
 print(estaOrdenada([5, 4, 3, 2, 1], False))
-print(estaOrdenada([3, 5, 2, 8], True))
+print(estaOrdenada([3, 5, 2, 8], False))
 print(estaOrdenada([10, 20, 15], False))
+
+print(estaOrdenada([1, 2, 3, 4, 5]))
+print(estaOrdenada([5, 4, 3, 2, 1]))
+print(estaOrdenada([3, 5, 2, 8]))
+print(estaOrdenada([10, 20, 15]))
