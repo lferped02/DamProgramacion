cded7cb839f5b37149d19c82d9378f630dc69be6
lferped02 <lidialilifernandezpedrosa@gmail.com>
2025-11-26 08:22:36 +0100

    Ejercicios Matrices

--- TEMA2/EJERCICIOS/Ejercicios Matrices/Ejercicio6 Matrices.py
+++ TEMA2/EJERCICIOS/Ejercicios Matrices/Ejercicio6 Matrices.py
@@ -1,12 +1,12 @@
 matriz=[[8, 1, 6],
         [3, 5, 7],
         [4, 9, 2]]
-
 def sumaColumna(matriz, indiceColumna):
     suma = 0
     for fila in matriz:
-        suma += fila[indiceColumna]
+        suma+=fila[indiceColumna]
     return suma
 
 # Programa principal
-print(sumaColumna(matriz, 1))
+resultado = sumaColumna(matriz, 0)#Suma la primera columna (índice 0)
+print(f"La suma de la columna es: {resultado}")
