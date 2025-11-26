5f35f484b2b2d076bdc4713491aa9d05f8a98692
lferped02 <lidialilifernandezpedrosa@gmail.com>
2025-11-26 09:50:25 +0100

    Ejercicios Matrices

--- TEMA2/EJERCICIOS/Ejercicios Matrices/Ejercicio10 Matrices.py
+++ TEMA2/EJERCICIOS/Ejercicios Matrices/Ejercicio10 Matrices.py
@@ -2,23 +2,15 @@ matriz=[[8, 1, 6],
         [3, 5, 7],
         [4, 9, 2]]
 
-#Diagonal principal con comprobacion
-def devuelveDiagonalPrincipal(matriz):
-    diagonal=[]
-    if len(matriz)==len(matriz[0]):
-        for i in range(len(matriz)):
-            diagonal.append(matriz[i][i])
-    return diagonal
-
-#Diagonal segundaria con comprobación
-def devuelveDiagonalSecundaria(matriz):
-    diagonal=[]
-    if len(matriz)==len(matriz[0]):
-        n=len(matriz)
-        for i in range(n):
-            diagonal.append(matriz[i][n-1-i])
-    return diagonal
+def diagonalSiCuadrada(matriz):
+    if len(matriz) != len(matriz[0]):
+        return False, []
+    
+    diagonal = []
+    for i in range(len(matriz)):
+        diagonal.append(matriz[i][i])
+    return True, diagonal
 
 #Programa principal
-print(devuelveDiagonalPrincipal(matriz))
-print(devuelveDiagonalSecundaria(matriz))
+print(diagonalSiCuadrada(matriz))
+print(diagonalSiCuadrada(matriz))  
