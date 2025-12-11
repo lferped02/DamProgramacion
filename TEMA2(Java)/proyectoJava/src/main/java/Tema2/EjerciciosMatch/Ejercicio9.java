93d7d8ad22d079d2ab3d70baac396fa6009c2ebd
lferped02 <lidialilifernandezpedrosa@gmail.com>
2025-12-11 19:10:49 +0100

    TEMA2(Java)

---
+++ TEMA2(Java)/proyectoJava/src/main/java/Tema2/EjerciciosMatch/Ejercicio9.java
@@ -0,0 +1,63 @@
+package EjerciciosMatch;
+
+import java.util.Scanner;
+
+public class Ejercicio9 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String opcion;
+        do {
+            System.out.println("========================================");
+            System.out.println("\t\tCALCULADORA");
+            System.out.println("========================================");
+            System.out.println("\t Introduzca + si desea sumar");
+            System.out.println("\t Introduzca - si desea restar");
+            System.out.println("\t Introduzca * si desea multiplicar");
+            System.out.println("\t Introduzca / si desea dividir");
+            System.out.println("\t Introduzca @ para salir del menú.");
+            System.out.println("========================================");
+            System.out.print("Elija una opción: ");
+            opcion = scanner.next();
+
+            switch (opcion) {
+                case "+":
+                    System.out.print("Introduzca un número: ");
+                    double numero1 = scanner.nextDouble();
+                    System.out.print("Introduzca otro número: ");
+                    double numero2 = scanner.nextDouble();
+                    System.out.println("Resultado: " + (numero1 + numero2));
+                    break;
+                case "-":
+                    System.out.print("Introduzca un número: ");
+                    numero1 = scanner.nextDouble();
+                    System.out.print("Introduzca otro número: ");
+                    numero2 = scanner.nextDouble();
+                    System.out.println("Resultado: " + (numero1 - numero2));
+                    break;
+                case "*":
+                    System.out.print("Introduzca un número: ");
+                    numero1 = scanner.nextDouble();
+                    System.out.print("Introduzca otro número: ");
+                    numero2 = scanner.nextDouble();
+                    System.out.println("Resultado: " + (numero1 * numero2));
+                    break;
+                case "/":
+                    System.out.print("Introduzca un número: ");
+                    numero1 = scanner.nextDouble();
+                    System.out.print("Introduzca otro número: ");
+                    numero2 = scanner.nextDouble();
+                    if (numero2 != 0) {
+                        System.out.println("Resultado: " + (numero1 / numero2));
+                    } else {
+                        System.out.println("Error: División entre cero no permitida.");
+                    }
+                    break;
+                case "@":
+                    System.out.println("Programa terminado. ¡Hasta pronto!");
+                    break;
+                default:
+                    System.out.println("Opción no válida. Intente de nuevo.");
+            }
+        }while (!opcion.equals("@"));
+    }
+}
---
+++ TEMA2(Java)/proyectoJava/src/main/java/Tema2/Unidad0/Ejercicio11.java
@@ -0,0 +1,23 @@
+package Tema2.Unidad0;
+
+import java.util.Scanner;
+
+public class Ejercicio11 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introduce el primer numero: ");
+        int numero1 = scanner.nextInt();
+        System.out.println("Introduce el segundo numero: ");
+        int numero2 = scanner.nextInt();
+        System.out.println("Introduce el tercer numero: ");
+        int numero3 = scanner.nextInt();
+
+        if(numero1>=numero2 && numero1>= numero3){
+            System.out.println("El mayor numero es: "+numero1);
+        }else if (numero2>=numero1 && numero2>=numero3) {
+            System.out.println("El mayor numero es: "+numero2);
+        }else{
+            System.out.println("El mayor numero es: "+numero3);
+        }
+    }
+}
