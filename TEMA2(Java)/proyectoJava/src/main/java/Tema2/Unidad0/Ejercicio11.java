986e1083efcf55d6aa514e614052b1c1927d2e5f
lferped02 <lidialilifernandezpedrosa@gmail.com>
2025-12-12 10:43:39 +0100

    TEMA2(Java)

--- TEMA2(Java)/proyectoJava/src/main/java/Tema2/Unidad0/Ejercicio11.java
+++ TEMA2(Java)/proyectoJava/src/main/java/Tema2/Unidad0/Ejercicio11.java
@@ -1,9 +1,9 @@
-package Tema2.Unidad0;
+package unidad2.Boletin0;
 
 import java.util.Scanner;
 
 public class Ejercicio11 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Introduce el primer numero: ");
         int numero1 = scanner.nextInt();
@@ -12,12 +12,12 @@ public class Ejercicio11 {
         System.out.println("Introduce el tercer numero: ");
         int numero3 = scanner.nextInt();
 
-        if(numero1>=numero2 && numero1>= numero3){
-            System.out.println("El mayor numero es: "+numero1);
-        }else if (numero2>=numero1 && numero2>=numero3) {
-            System.out.println("El mayor numero es: "+numero2);
-        }else{
-            System.out.println("El mayor numero es: "+numero3);
+        if (numero1 >= numero2 && numero1 >= numero3) {
+            System.out.println("El mayor numero es: " + numero1);
+        } else if (numero2 >= numero1 && numero2 >= numero3) {
+            System.out.println("El mayor numero es: " + numero2);
+        } else {
+            System.out.println("El mayor numero es: " + numero3);
         }
     }
 }
