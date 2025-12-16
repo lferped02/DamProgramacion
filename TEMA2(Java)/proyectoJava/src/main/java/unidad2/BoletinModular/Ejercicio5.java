596d656f773f1dfa1ae269951e2cac7096839c02
lferped02 <lidialilifernandezpedrosa@gmail.com>
2025-12-16 09:36:21 +0100

    TEMA2(java)

--- TEMA2(Java)/proyectoJava/src/main/java/unidad2/BoletinModular/Ejercicio5.java
+++ TEMA2(Java)/proyectoJava/src/main/java/unidad2/BoletinModular/Ejercicio5.java
@@ -1,4 +1,13 @@
 package unidad2.BoletinModular;
 
 public class Ejercicio5 {
+    public boolean descuento(boolean premium, int edad, float gasto){
+        boolean descuento = False;
+
+        return descuento;
+    }
+    public static void main() {
+        Ejercicio5 referencia = new Ejercicio5();
+        boolean tiene1= referencia.descuento(True, 30,1.2f);
+    }
 }
