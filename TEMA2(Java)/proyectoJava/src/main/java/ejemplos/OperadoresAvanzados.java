55a5b4a592ef83fff5fbed10bbadaca50de3c474
lferped02 <lidialilifernandezpedrosa@gmail.com>
2025-12-17 09:24:34 +0100

    TEMA 2(java)

---
+++ TEMA2(Java)/proyectoJava/src/main/java/ejemplos/OperadoresAvanzados.java
@@ -0,0 +1,13 @@
+package ejemplos;
+
+public class OperadoresAvanzados {
+    public static void main() {
+        int x=23;
+        int y=33;
+        int z=43;
+        int resultado = (x > y) ?
+                (x > z ? x : z)
+                : (y > z ? y : z);
+        System.out.println(resultado);
+    }
+}
