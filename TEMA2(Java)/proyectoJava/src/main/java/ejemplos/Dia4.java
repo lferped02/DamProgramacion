057eb402fc4c850cbe89d08e9a566b5d9d3e5d86
lferped02 <lidialilifernandezpedrosa@gmail.com>
2026-01-08 09:33:46 +0100

    TEMA 2(java)

---
+++ TEMA2(Java)/proyectoJava/src/main/java/ejemplos/Dia4.java
@@ -0,0 +1,21 @@
+package ejemplos;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Dia4 {
+    public static void main(String[] args) {
+        boolean encontrado = false;
+        Random random = new Random();
+        int numeroAlzar = random.nextInt(10);
+        Scanner scanner = new Scanner(System.in);
+        int apuesta;
+        while (!encontrado){
+            System.out.println("Adivina el numero: ");
+            apuesta = scanner.nextInt();
+            if(apuesta == numeroAlzar){
+                encontrado = true;
+            }
+        }
+    }
+}
