a776b9985a518fce809d60b601ad3b908c4fe3ea
lferped02 <lidialilifernandezpedrosa@gmail.com>
2026-01-08 09:59:51 +0100

    Add files via upload

--- TEMA2(Java)/proyectoJava/src/main/java/unidad2/BoletinModular/Ejercicio7.java
+++ TEMA2(Java)/proyectoJava/src/main/java/unidad2/BoletinModular/Ejercicio7.java
@@ -1,13 +1,19 @@
 package unidad2.BoletinModular;
 
+import java.util.Scanner;
+
 public class Ejercicio7 {
     public void imprimeMultiplos(int numero) {
-        for (int i = numero; i <= numero*10; i += numero) {
-            System.out.println(i);
+        for (int i=1; i<11; i++) {
+            int multiplo = i*numero;
+            System.out.println(multiplo);
         }
     }
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introduce el numero: ");
+        int numero = scanner.nextInt();
         Ejercicio7 referencia = new Ejercicio7();
         referencia.imprimeMultiplos(3);
     }
