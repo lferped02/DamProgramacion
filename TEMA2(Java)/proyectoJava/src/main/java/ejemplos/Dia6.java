9dc74290e55412313cb637a2402b0dd26ed70b60
lferped02 <lidialilifernandezpedrosa@gmail.com>
2026-01-21 08:47:25 +0100

    TEMA 2(java)

---
+++ TEMA2(Java)/proyectoJava/src/main/java/ejemplos/Dia6.java
@@ -0,0 +1,11 @@
+package ejemplos;
+
+public class Dia6 {
+    public static void main(String[] args) {
+        String [][] matriz = new String[3][4];//El primer[filas] y el segundo [columnas]
+        for (int i=0; i < matriz.length;i++){
+            String[]fila = matriz[i];
+            //Recorrido de tablas
+        }
+    }
+}
