fe0a5614a701459502e4207afab7f78e709d5116
lferped02 <lidialilifernandezpedrosa@gmail.com>
2026-01-30 12:06:28 +0100

    Orientacion a Objetos

---
+++ TEMA2(Java)/proyectoJava/src/main/java/OrientacionAObjetos/Notas/Alumno.java
@@ -0,0 +1,29 @@
+package OrientacionAObjetos.Notas;
+
+public class Alumno {
+    String nombre;
+    String apellidos;
+    String email;
+    NotaAsignatura notasProgramacion;
+    NotaAsignatura notasBaseDatos;
+
+    public Alumno(String nombre, String apellidos, String email, NotaAsignatura notasProg, NotaAsignatura notasBD) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.email = email;
+        this.notasProgramacion = notasProg;
+        this.notasBaseDatos = notasBD;
+    }
+    public void imprimeSiHaAprobado(NotaAsignatura asignatura) {
+        if (asignatura.calcularMedia() >= 5) {
+            System.out.println("El alumno " + nombre + " ha aprobado la asignatura " + asignatura.nombreAsignatura);
+        } else {
+            System.out.println("El alumno " + nombre + " NO ha aprobado la asignatura " + asignatura.nombreAsignatura);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Alumno: " + nombre + " " + apellidos + " (" + email + ")\n  > " + notasProgramacion + "\n  > " + notasBaseDatos;
+    }
+}
---
+++ TEMA2(Java)/proyectoJava/src/main/java/OrientacionAObjetos/Notas/NotaAsignatura.java
@@ -0,0 +1,30 @@
+package OrientacionAObjetos.Notas;
+
+public class NotaAsignatura {
+    String nombreAsignatura;
+    double notaT1;
+    double notaT2;
+    double notaT3;
+
+    public NotaAsignatura(String nombreAsignatura, double notaT1, double notaT2, double notaT3) {
+        this.nombreAsignatura = nombreAsignatura;
+        this.notaT1 = notaT1;
+        this.notaT2 = notaT2;
+        this.notaT3 = notaT3;
+    }
+    public double calcularMedia() {
+        return (notaT1 + notaT2 + notaT3) / 3.0;
+    }
+    public int trimestresAprobados() {
+        int contador = 0;
+        if (notaT1 >= 5) contador++;
+        if (notaT2 >= 5) contador++;
+        if (notaT3 >= 5) contador++;
+        return contador;
+    }
+
+    @Override
+    public String toString() {
+        return nombreAsignatura + " [T1: " + notaT1 + ", T2: " + notaT2 + ", T3: " + notaT3 + "]";
+    }
+}
