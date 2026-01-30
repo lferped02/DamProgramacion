c67411ac653eda7389b6b3a446e776e9aa1f9d66
lferped02 <lidialilifernandezpedrosa@gmail.com>
2026-01-30 10:12:01 +0100

    Orientacion a Objetos

--- TEMA2(Java)/proyectoJava/src/main/java/OrientacionAObjetos/Pokemon.java
+++ TEMA2(Java)/proyectoJava/src/main/java/OrientacionAObjetos/Pokemon.java
@@ -7,7 +7,7 @@ public class Pokemon {
     int nivelVida;
     String nombreEvolucion;
 
-    // Constructor
+    // Constructorres.
     public Pokemon(String nombre, String tipo, int nivelVida, String nombreEvolucion) {
         this.nombre = nombre;
         this.tipo = tipo;
@@ -18,7 +18,6 @@ public class Pokemon {
     boolean luchar(Pokemon adversario) {
         return this.nivelVida > adversario.nivelVida;
     }
-
     void evolucionar() {
         if (nombreEvolucion != null) {
             this.nombre = this.nombreEvolucion;
