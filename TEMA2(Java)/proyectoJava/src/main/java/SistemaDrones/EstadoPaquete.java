272573d8d0980fd0b02694a7731f68027b0956b5
lferped02 <lidialilifernandezpedrosa@gmail.com>
2026-02-04 13:24:15 +0100

    Enum Orientacion

---
+++ TEMA2(Java)/proyectoJava/src/main/java/SistemaDrones/EstadoPaquete.java
@@ -0,0 +1,5 @@
+package EnumOrientacion.SistemaDrones;
+
+public enum EstadoPaquete {
+    ENPREPARACION, ENREPARTO, RETRASADO, CANCELADO
+}
---
+++ TEMA2(Java)/proyectoJava/src/main/java/SistemaDrones/Ruta.java
@@ -0,0 +1,28 @@
+package EnumOrientacion.SistemaDrones;
+
+class Ruta {
+    public String origen;
+    public String destino;
+    public double pesoPaquete;
+    public EstadoPaquete estadoPaquete;
+
+    public Ruta(String origen, String destino, double pesoPaquete) {
+        this.origen = origen;
+        this.destino = destino;
+        this.pesoPaquete = pesoPaquete;
+        this.estadoPaquete = EstadoPaquete.ENPREPARACION;
+    }
+
+    public void setEstadoPaquete(EstadoPaquete nuevoEstado) {
+        this.estadoPaquete = nuevoEstado;
+    }
+
+    public EstadoPaquete getEstadoPaquete() {
+        return estadoPaquete;
+    }
+
+    @Override
+    public String toString() {
+        return "Ruta a " + destino + " [" + estadoPaquete + "]";
+    }
+}
