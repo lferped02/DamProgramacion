7dedda76635b5a2a4dca97aa8f9c74f1f1790899
lferped02 <lidialilifernandezpedrosa@gmail.com>
2026-02-05 08:20:01 +0100

    Enum Orientacion

--- TEMA2(Java)/proyectoJava/src/main/java/EnumOrientacion/GestionRobotsLimpieza/GestionaRobots.java
+++ TEMA2(Java)/proyectoJava/src/main/java/EnumOrientacion/GestionRobotsLimpieza/GestionaRobots.java
@@ -2,21 +2,21 @@ package EnumOrientacion.GestionRobotsLimpieza;
 
 public class GestionaRobots {
     public static void main(String[] args) {
-        Habitacion habitacion = new Habitacion("Sala de Juntas", 50.5, 80);
-        Habitacion habitacion2 = new Habitacion("Laboratorio 1", 30.0, 40);
-        Habitacion habitacion3 = new Habitacion("Oficina Principal", 20.0, 10);
+        Habitacion h = new Habitacion("Sala de Juntas", 50.5, 80);
+        Habitacion h2 = new Habitacion("Laboratorio 1", 30.0, 40);
+        Habitacion h3 = new Habitacion("Oficina Principal", 20.0, 10);
 
         Robot robbie = new Robot("ROB-01", ModoRobot.AUTO);
         Robot dustin = new Robot("ROB-02", ModoRobot.MANUAL);
 
         System.out.println("--- Iniciando tareas de limpieza ---");
-        robbie.asignarHabitacion(habitacion);
+        robbie.asignarHabitacion(h);
 
-        habitacion2.setEstado(EstadoHabitacion.BLOQUEADA);
-        System.out.println("Estado actual de " + habitacion2);
+        h2.setEstado(EstadoHabitacion.BLOQUEADA);
+        System.out.println("Estado actual de " + h2);
 
         System.out.println("\n--- Intento de asignación inválida ---");
-        dustin.asignarHabitacion(habitacion3);
+        dustin.asignarHabitacion(h3);
 
         robbie.vaciarDeposito();
         dustin.recargar();
