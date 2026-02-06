e16b16551da83bb3f14175397dfbe2398282fbf4
lferped02 <lidialilifernandezpedrosa@gmail.com>
2026-02-06 10:57:07 +0100

    Enum Orientacion

---
+++ TEMA2(Java)/proyectoJava/src/main/java/EnumOrientacion/PartidaJuegoMesa/Partida.java
@@ -0,0 +1,58 @@
+package EnumOrientacion.PartidaJuegoMesa;
+
+import java.util.Arrays;
+
+public class Partida {
+    private String nombreJuego;
+    private Participante[] participantes;
+    private Participante ganador;
+
+    public Partida() {
+        this.participantes = new Participante[4];
+    }
+
+    public String getNombreJuego() {
+        return nombreJuego;
+    }
+
+    public void setNombreJuego(String nombreJuego) {
+        this.nombreJuego = nombreJuego;
+    }
+
+    public Participante[] getParticipantes() {
+        return participantes;
+    }
+
+    public void setParticipantes(Participante[] participantes) {
+        this.participantes = participantes;
+    }
+
+    public Participante getGanador() {
+        return ganador;
+    }
+
+    public void setGanador(Participante ganador) {
+        this.ganador = ganador;
+    }
+    public Participante calculaParticipanteMasPuntos() {
+        int puntuacion = 0;
+        Participante p1 = participantes[0];
+        for (int i =0; i< participantes.length; i++){
+            Participante p = this.participantes[i];
+            if(p.getPuntuacion()>puntuacion){
+                puntuacion = p.getPuntuacion();
+                p1 = p;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return "Partida{" +
+                "nombreJuego='" + nombreJuego + '\'' +
+                ", participantes=" + Arrays.toString(participantes) +
+                ", ganador=" + ganador +
+                '}';
+    }
+}
