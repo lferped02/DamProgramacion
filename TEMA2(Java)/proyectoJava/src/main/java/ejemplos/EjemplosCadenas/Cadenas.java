6985e9df1bdd540226fcb62876a216ad99138df6
lferped02 <lidialilifernandezpedrosa@gmail.com>
2026-02-12 09:37:55 +0100

    Ejemplos

---
+++ TEMA2(Java)/proyectoJava/src/main/java/ejemplos/EjemplosCadenas/Cadenas.java
@@ -0,0 +1,9 @@
+package ejemplos.EjemplosCadenas;
+
+public class Cadenas {
+    public static void main(String[] args) {
+        char letra= 'a';
+        Character letra1 = 'a';
+
+    }
+}
---
+++ TEMA2(Java)/proyectoJava/src/main/java/ejemplos/EjemplosDias/Dia5.java
@@ -0,0 +1,10 @@
+package ejemplos.EjemplosDias;
+
+public class Dia5 {
+    public static void main(String[] args) {
+        String cadena = "Hola";
+        String cadena2 = "Adiós";
+        cadena.concat(cadena2);
+        System.out.println(cadena);
+    }
+}
