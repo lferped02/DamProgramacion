a0afacd9b959bf43777f9e76752aebbce7da6c2b
lferped02 <lidialilifernandezpedrosa@gmail.com>
2026-02-12 09:29:38 +0100

    Ejemplo Herencia

---
+++ TEMA2(Java)/proyectoJava/src/main/java/ejemplos/EjemploSuma/Suma.java
@@ -0,0 +1,13 @@
+package ejemplos.EjemploSuma;
+
+public class Suma {
+    //Sobrecargar.
+    int suma (int x, int y){
+        return x + y;
+    }
+    double suma (int y) {
+        double d=0;
+        return d;
+    }
+
+}
