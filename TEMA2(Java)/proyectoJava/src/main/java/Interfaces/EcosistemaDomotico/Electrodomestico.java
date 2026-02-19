b02447b3c237906711cc065fcc682662f4523210
lferped02 <lidialilifernandezpedrosa@gmail.com>
2026-02-19 19:06:27 +0100

    Interfaces

---
+++ TEMA2(Java)/proyectoJava/src/main/java/Interfaces/EcosistemaDomotico/Electrodomestico.java
@@ -0,0 +1,22 @@
+package Interfaces.EcosistemaDomotico;
+
+public abstract class Electrodomestico extends Dispositivo implements Inteligente{
+    protected float consumoEnergetico;
+
+    public Electrodomestico(String codigoSerie, String marca, float consumoEnergetico) {
+        super(codigoSerie, marca);
+        if (consumoEnergetico < 0) {
+            consumoEnergetico = 0;
+        }
+        this.consumoEnergetico = consumoEnergetico;
+    }
+
+    public void encender(){
+        this.estado = Estado.ESCENDIDO;
+        System.out.println("El electrodoméstico está encendido.");
+    }
+    public void apagar(){
+        this.estado = Estado.APAGADO;
+        System.out.println("El electrodoméstico está apagado.");
+    }
+}
---
+++ TEMA2(Java)/proyectoJava/src/main/java/Interfaces/EcosistemaDomotico/Inteligente.java
@@ -0,0 +1,6 @@
+package Interfaces.EcosistemaDomotico;
+
+public interface Inteligente extends Conectable{
+    void encender();
+    void apagar();
+}
