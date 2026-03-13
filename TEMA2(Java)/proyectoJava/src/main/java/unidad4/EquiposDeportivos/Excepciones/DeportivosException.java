7877200e2e43f450bc4b607bc5817264d9ebfb1c
lferped02 <lidialilifernandezpedrosa@gmail.com>
2026-03-13 12:08:30 +0100

    Delete TEMA2(Java)/proyectoJava/src/main/java/unidad4 directory

--- TEMA2(Java)/proyectoJava/src/main/java/unidad4/EquiposDeportivos/Excepciones/DeportivosException.java
+++
@@ -1,7 +0,0 @@
-package unidad4.EquiposDeportivos.Excepciones;
-
-public class DeportivosException extends Exception {
-    public DeportivosException(String mensaje) {
-        super(mensaje);
-    }
-}
--- TEMA2(Java)/proyectoJava/src/main/java/unidad4/GestionDeEstudiantes/Excepciones/EstudianteNoEncontradoException.java
+++
@@ -1,7 +0,0 @@
-package unidad4.GestionDeEstudiantes.Excepciones;
-
-public class EstudianteNoEncontradoException extends Exception {
-    public EstudianteNoEncontradoException(String mensaje) {
-        super(mensaje);
-    }
-}
