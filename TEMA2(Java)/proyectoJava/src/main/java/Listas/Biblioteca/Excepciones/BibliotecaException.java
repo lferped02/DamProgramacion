4f2464b2c0c5fb4b1582259ac36f053a5c10c368
lferped02 <lidialilifernandezpedrosa@gmail.com>
2026-03-18 09:49:22 +0100

    Delete TEMA2(Java)/proyectoJava/src/main/java/Listas directory

--- TEMA2(Java)/proyectoJava/src/main/java/Listas/Biblioteca/Excepciones/BibliotecaException.java
+++
@@ -1,7 +0,0 @@
-package unidad4.Listas.Biblioteca.Excepciones;
-
-public class BibliotecaException extends RuntimeException {
-    public BibliotecaException(String message) {
-        super(message);
-    }
-}
--- TEMA2(Java)/proyectoJava/src/main/java/Listas/Biblioteca/Modelo/Estado.java
+++
@@ -1,5 +0,0 @@
-package unidad4.Listas.Biblioteca.Modelo;
-
-public enum Estado {
-    PRESTADO, LIBRE;
-}
