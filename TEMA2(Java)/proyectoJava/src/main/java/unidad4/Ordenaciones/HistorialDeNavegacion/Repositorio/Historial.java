435b361c43205f39ab2c7a2fe9caa9fc8cb32fd1
lferped02 <lidialilifernandezpedrosa@gmail.com>
2026-03-20 11:35:01 +0100

    Ordenaciones

--- TEMA2(Java)/proyectoJava/src/main/java/unidad4/Ordenaciones/HistorialDeNavegacion/Repositorio/Historial.java
+++ TEMA2(Java)/proyectoJava/src/main/java/unidad4/Ordenaciones/HistorialDeNavegacion/Repositorio/Historial.java
@@ -4,6 +4,7 @@ import unidad4.Ordenaciones.HistorialDeNavegacion.Excepciones.HistorialException
 import unidad4.Ordenaciones.HistorialDeNavegacion.Modelo.PaginaWeb;
 
 import java.time.LocalDate;
+import java.util.HashSet;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -22,34 +23,63 @@ public class Historial {
         if (fecha.isAfter(LocalDate.now())) {
             throw new HistorialException("Fecha futura no permitida");
         }
-
         historial.add(new PaginaWeb(url, fecha));
     }
 
     public void mostrarHistorial() {
+        if (historial.isEmpty()) {
+            System.out.println("Historial vacío.");
+            return;
+        }
+
         for (PaginaWeb p : historial) {
             System.out.println(p);
         }
     }
 
     public void consultarPorFecha(LocalDate fecha) {
+        boolean encontrado = false;
+
         for (PaginaWeb p : historial) {
             if (p.getFecha().equals(fecha)) {
                 System.out.println(p);
+                encontrado = true;
             }
+        }
+
+        if (!encontrado) {
+            System.out.println("No hay registros en esa fecha.");
         }
     }
 
     public void consultarPorUrl(String url) {
+        boolean encontrado = false;
+
         for (PaginaWeb p : historial) {
             if (p.getUrl().equalsIgnoreCase(url)) {
                 System.out.println(p);
+                encontrado = true;
             }
+        }
+        if (!encontrado) {
+            System.out.println("No hay registros para esa URL.");
         }
     }
 
     public void borrarPorUrl(String url) {
-        historial.removeIf(p -> p.getUrl().equalsIgnoreCase(url));
+        Set<PaginaWeb> paginasBorrar = new HashSet<>();
+        for (PaginaWeb p : historial) {
+            if (p.getUrl().equalsIgnoreCase(url)) {
+                paginasBorrar.add(p);
+            }
+        }
+
+        if (paginasBorrar.isEmpty()) {
+            System.out.println("No hay páginas para borrar.");
+        } else {
+            historial.removeAll(paginasBorrar);
+            System.out.println("Páginas eliminadas correctamente.");
+        }
     }
 
     public Set<PaginaWeb> getHistorial() {
