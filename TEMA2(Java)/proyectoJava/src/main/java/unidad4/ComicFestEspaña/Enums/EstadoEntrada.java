61c52759ce6808f8db454fcf9ed7fccd62d2b426
lferped02 <lidialilifernandezpedrosa@gmail.com>
2026-03-26 17:52:32 +0100

    Delete TEMA2(Java)/proyectoJava/src/main/java/unidad4/ComicFestEspaña directory

--- TEMA2(Java)/proyectoJava/src/main/java/unidad4/ComicFestEspaña/Enums/EstadoEntrada.java
+++
@@ -1,5 +0,0 @@
-package unidad4.Mapas.ComicFestEspaña.Enums;
-
-public enum EstadoEntrada {
-    ENVIADA, CONFIRMADA, CANCELADA;
-}
--- TEMA2(Java)/proyectoJava/src/main/java/unidad4/ComicFestEspaña/Enums/TipoEntrada.java
+++
@@ -1,5 +0,0 @@
-package unidad4.Mapas.ComicFestEspaña.Enums;
-
-public enum TipoEntrada {
-    SILVER, GOLD, PLATINIUM;
-}
